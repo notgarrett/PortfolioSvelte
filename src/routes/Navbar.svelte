c13309b7ec189770b85b603baec6ffd2c9d9c4e2
notgarrett <garrettrgiles@gmail.com>
2023-03-23 16:28:51 -0400

    Slight updates to the nav bar, nothing special.

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,32 +1,37 @@
-<nav
-	class="relative flex w-full flex-nowrap items-center justify-between bg-neutral-100 py-4 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 lg:flex-wrap lg:justify-start"
-	data-te-navbar-ref>
-	<div class="flex w-full flex-wrap items-center justify-between px-6">
-
-		<div
-			class="!visible hidden flex-grow basis-[100%] items-center justify-center lg:!flex lg:basis-auto"
-			id="navbarSupportedContent3"
-			data-te-collapse-item>
-			<!-- Left links -->
-			<ul
-				class="list-style-none mr-auto flex flex-col pl-0 lg:flex-row justify-center"
-				data-te-navbar-nav-ref>
-				<li class="lg:px-2" data-te-nav-item-ref>
-					<a
-						class="active disabled:text-black/30 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-						aria-current="page"
-						href="blog"
-						data-te-nav-link-ref
-					>blog</a
-					>
-				</li>
-				<li>
-					<a href="/">test</a>
-				</li>
-
-			</ul>
-			<!-- Left links -->
-		</div>
-		<!-- Collapsible wrapper -->
+<!-- Nav Start -->
+<nav>
+	<div>
+		<h1>
+			<a>Test1</a>
+		</h1>
+	</div>
+	<div>
+		<h1>
+			<a href="#">
+				<span> Home </span>
+			</a>
+		</h1>
+	</div>
+	<div>
+		<h1>
+			<a href="#">
+				<span> Projects </span>
+			</a>
+		</h1>
+	</div>
+	<div>
+		<h1>
+			<a href="#">
+				<span> Blog </span>
+			</a>
+		</h1>
+	</div>
+	<div>
+		<h1>
+			<a href="#">
+				<span> Contact </span>
+			</a>
+		</h1>
 	</div>
 </nav>
+<!-- end nav -->
